a1b2279a096649b14a333e37e4f2c4981e48af29
Pat Neel <patrick.neel@colorado.edu>
2017-12-19 12:53:23 -0700

    added tests and lodash to project

---
+++ src/unit-tests/iteration-0.test.js
@@ -0,0 +1,10 @@
+import helper from '../helper/HelperFunctions';
+
+const { range } = helper;
+
+describe('Range Test', () => {
+
+  test('should be defined', () => {
+    expect(range).toBe('func')
+  });
+})
