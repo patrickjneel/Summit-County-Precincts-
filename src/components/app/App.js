ad47182559c322611e0fd0dbb525f671525f68aa
Pat Neel <patrick.neel@colorado.edu>
2017-12-27 15:31:32 -0700

    added map and fixed stupidity

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Search from '../search/Search';
 import MapArea from '../../containers/map-area/MapArea';
 import './app.css';
-import Papa from 'papaparse'
 
 class App extends Component {
 
--- src/containers/map-area/MapArea.js
+++ src/containers/map-area/MapArea.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 import './map-area.css';
 
 
 class MapArea extends Component {
+  
 
-render () {
-  return (
-    <div className="map-area">
-    </div>
-    )
+  render() {
+    return (
+      <div className="map-area">
+      <GoogleMapReact
+        defaultCenter={{lat: 39.5, lng: -106.06}}
+        defaultZoom={11}
+      />
+      </div>
+    );
   }
 }
 
