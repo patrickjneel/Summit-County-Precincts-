eefe02f89ee657281d7e738ff5f4b6c7edc96927
Pat Neel <patrick.neel@colorado.edu>
2018-01-02 15:38:27 -0700

    added font

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import Helper from '../../helper/HelperFunctions';
-import parsedData from '../../helper/precinct8';
 import Search from '../search/Search';
 import MapArea from '../map-area/MapArea';
 import Data from '../../containers/data/Data';
-import './app.css';
-
-// const helper = new Helper(parsedData)
+import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-    // console.log('ok?') 
-  }
 
   render() {
 
