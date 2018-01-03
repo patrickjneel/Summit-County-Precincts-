0ceef7fdde58c14b57ef2ad3bf44227a2ce99ffe
Pat Neel <patrick.neel@colorado.edu>
2018-01-02 18:53:36 -0700

    added different maps

--- src/components/map-area/MapArea.js
+++ src/components/map-area/MapArea.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
 import './map-area.css';
+import {withGoogleMap, GoogleMap, Marker} from "react-google-maps";
+
+const MapWithAMarker = withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={11}
+    defaultCenter={{ lat: 39.59, lng: -106.06 }}
+  >
+    <Marker
+      position={{ lat: 39.59, lng: -106.06 }}
+    />
+  </GoogleMap>
+);
+
 
 
 const MapArea = () => {
 
     return (
       <div className="map-area">
-      <GoogleMapReact
-        defaultCenter={{lat: 39.5, lng: -106.06}}
-        defaultZoom={11}
+      <MapWithAMarker
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
       />
       </div>
     );
