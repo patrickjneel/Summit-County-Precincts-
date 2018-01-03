c8d0e108744075c550224c61772071037b7b4ce0
Pat Neel <patrick.neel@colorado.edu>
2018-01-03 10:53:44 -0700

    added tests for components

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-it.only('should be defined', () => {
-  const renderedApp = shallow(<App />);
+describe('App Tests', () => {
 
-  expect(renderedApp).toBeDefined();
+  it('should be defined', () => {
+    const renderedApp = shallow(<App />);
+
+    expect(renderedApp).toBeDefined();
+  });
+
+  it('should match the snap shot', () => {
+    const renderedApp = shallow(<App />);
+
+    expect(renderedApp).toMatchSnapshot();
+  });
+
 });
