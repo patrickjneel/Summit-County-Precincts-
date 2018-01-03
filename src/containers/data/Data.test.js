3ca620fdbf6f5e8580753bab09ac2c38a9f5f09a
Pat Neel <patrick.neel@colorado.edu>
2018-01-03 09:51:07 -0700

    added mapStateToProps test for data

---
+++ src/containers/data/Data.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Data, mapStateToProps, mapDispatchToProps } from './Data';
+
+
+describe('MapStateToProps Test', () => {
+  it('should receive data from the store', () => {
+    const mockStore = {
+      data: [
+      {
+        county:"SUMMIT",
+        HouseFrom: 701,
+        houseTo: 889,
+        ruleType: 0,
+        fullAddress: [{house: '701 e annemone trl'}],
+        precinct: 2086159008,
+        splitCode: 26
+      }
+      ]
+    }
+    const expected = mapStateToProps(mockStore)
+    expect(expected.data).toEqual(mockStore.data)
+  })
+})
