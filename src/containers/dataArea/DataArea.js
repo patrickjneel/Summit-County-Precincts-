75ea306beb4b4867e015b50d7640380d4c0bec72
Pat Neel <patrick.neel@colorado.edu>
2018-01-05 12:19:05 -0700

    fixed styling for container

--- src/containers/dataArea/DataArea.js
+++ src/containers/dataArea/DataArea.js
@@ -9,9 +9,17 @@ render() {
   return (
       <div className="data-area">
         <h2>Home Address Precinct Information</h2>
+        <table>
+          <tr>
+            <td className="key">Address:</td>
+            <td className="value">Expected Location</td>
+          </tr>
+        </table>
       </div>
     )
   }
 }
 
 export default DataArea;
+ 
+ //add table to display the information
