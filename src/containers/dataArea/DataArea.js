b8a33e511ea754eb69e8b5e0cb79bd20ee79cdd8
Pat Neel <patrick.neel@colorado.edu>
2018-01-05 11:22:33 -0700

    added dataArea component and styling for the container

---
+++ src/containers/dataArea/DataArea.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './data-area.css';
+
+
+class DataArea extends Component {
+
+render() {
+  return (
+      <div className="data-area">
+        Shit goes here
+      </div>
+    )
+  }
+}
+
+export default DataArea;
