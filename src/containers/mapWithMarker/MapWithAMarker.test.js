0db6781ecf03730754877f48bafdb3ec60fa380f
Pat Neel <patrick.neel@colorado.edu>
2018-01-07 19:26:34 -0700

    testing

--- src/containers/mapWithMarker/MapWithAMarker.test.js
+++ src/containers/mapWithMarker/MapWithAMarker.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MapWithAMarker, mapStateToProps, mapDispatchToProps } from './MapWithAMarker';
+import { MapWithAMarker, mapStateToProps } from './MapWithAMarker';
+
+
 
 describe('Map State To Props Test', () => {
-
+  it('should receive location data from the store', () => {
+    const mockStore = {
+      coords: {
+        address: "Buffalo St",
+        lat: 39.05,
+        lng: -106.06,
+        fullAddress: '200 Buffalo St Dillon, CO'
+      }
+    };
+    const expected = mapStateToProps(mockStore);
+    expect(expected.coords).toEqual(mockStore.coords)
+  })
 });
